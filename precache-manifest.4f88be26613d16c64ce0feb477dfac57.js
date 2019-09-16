53225a64cccf16bc609398f8917f5fa03f53c7b4
HardRye <nikolaimihail@gmail.com>
2019-09-16 23:30:32 +0300

    Updates

---
+++ precache-manifest.4f88be26613d16c64ce0feb477dfac57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f236d740c127b78550d35feac38d5efc",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e62a28ffe8958da1f1c9",
+    "url": "/goit-react-hw-01-components/static/css/main.c9cc4b46.chunk.css"
+  },
+  {
+    "revision": "c401c12a6dae5c6e05e8",
+    "url": "/goit-react-hw-01-components/static/js/2.60faa2b2.chunk.js"
+  },
+  {
+    "revision": "e62a28ffe8958da1f1c9",
+    "url": "/goit-react-hw-01-components/static/js/main.eb0f52a7.chunk.js"
+  },
+  {
+    "revision": "620ceb3fc25e2e9c8f81",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.a6ab4004.js"
+  }
+]);
